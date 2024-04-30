fb39147ece700bef66b2f620d67d8aa642d1c5e6
AdrianBailador <adrianbailador@hotmail.com>
2024-04-30 10:33:34 +0100

    Facade Pattern

---
+++ FacadePattern/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace FacadePattern
+{
+    public class SubSystemOne
+    {
+        public void MethodOne()
+        {
+            Console.WriteLine("SubSystemOne Method");
+        }
+    }
+
+    public class SubSystemTwo
+    {
+        public void MethodTwo()
+        {
+            Console.WriteLine("SubSystemTwo Method");
+        }
+    }
+
+    public class SubSystemThree
+    {
+        public void MethodThree()
+        {
+            Console.WriteLine("SubSystemThree Method");
+        }
+    }
+
+    public class Facade
+    {
+        private SubSystemOne one;
+        private SubSystemTwo two;
+        private SubSystemThree three;
+
+        public Facade()
+        {
+            one = new SubSystemOne();
+            two = new SubSystemTwo();
+            three = new SubSystemThree();
+        }
+
+        public void MethodA()
+        {
+            Console.WriteLine("\nMethodA() ---- ");
+            one.MethodOne();
+            two.MethodTwo();
+        }
+
+        public void MethodB()
+        {
+            Console.WriteLine("\nMethodB() ---- ");
+            two.MethodTwo();
+            three.MethodThree();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Facade facade = new Facade();
+
+            facade.MethodA();
+            facade.MethodB();
+        }
+    }
+}

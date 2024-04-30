0bc5836ae713b2e70bb89f7858f6c5bb3d8b9b35
AdrianBailador <adrianbailador@hotmail.com>
2024-04-30 10:34:46 +0100

    Bridge Pattern

---
+++ BridgePattern/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace BridgePattern
+{
+    public interface IImplementation
+    {
+        string OperationImplementation();
+    }
+
+    public class Abstraction
+    {
+        protected IImplementation implementation;
+
+        public Abstraction(IImplementation implementation)
+        {
+            this.implementation = implementation;
+        }
+
+        public virtual string Operation()
+        {
+            return "Abstract: Base operation with:\n" +
+                implementation.OperationImplementation();
+        }
+    }
+
+    public class ConcreteImplementationA : IImplementation
+    {
+        public string OperationImplementation()
+        {
+            return "ConcreteImplementationA: Here's the result on the platform A.";
+        }
+    }
+
+    public class ConcreteImplementationB : IImplementation
+    {
+        public string OperationImplementation()
+        {
+            return "ConcreteImplementationB: Here's the result on the platform B.";
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Abstraction abstraction = new Abstraction(new ConcreteImplementationA());
+            Console.WriteLine(abstraction.Operation());
+
+            abstraction = new Abstraction(new ConcreteImplementationB());
+            Console.WriteLine(abstraction.Operation());
+        }
+    }
+}
